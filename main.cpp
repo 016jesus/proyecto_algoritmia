0c37d83b3e664196a5f15b726d80778b11276562
Jesus <113791040+016jesus@users.noreply.github.com>
2023-06-17 20:39:44 -0500

    Actualizar main.cpp
    Cree la función de volúmenes con sus respectivos cálculos 
    Declare la variable "opc" como global para que el valor de esta defina la ejecución del código 
    Antes de hacer la llamada a main() se debe redefinir el valor de "opc" según el menú o submenú que se quiera mostrar
    //opc = 0 muestra el menú principal

--- main.cpp
+++ main.cpp
@@ -3,19 +3,25 @@
 
 using namespace std;
 
-void areas_(int opc);
-void volumenes_(int opc);
-void temperaturas_(int opc);
-void monedas_(int opc);
-void numericos_(int opc);
-void arreglos_matrices(int opc);
-void varios_(int opc);
+void areas_(int &opc);
+void volumenes_(int &opc);
+void temperaturas_(int &opc);
+void monedas_(int &opc);
+void numericos_(int &opc);
+void arreglos_matrices(int &opc);
+void varios_(int &opc);
 
+void opcion_invalida();
+
+int opc=0;
 
 int main(){
+    system("cls");
     
-int opc=0;
-system("cls");
+    if (opc != 0){
+    goto salto;
+    }
+
     //menu principal
         cout<<"Menu principal"
               "\n1. Areas"
@@ -28,9 +34,13 @@ system("cls");
               "\n8. Salir"
               "\nIngrese una opcion: ";
             cin>>opc;          
+           
+            salto:
+            
    switch(opc){     
                 case 1:         
                 system("cls");
+                 cout<<opc;
   cout<<"Sub-menu areas"
               "\n1. Triangulo"
               "\n2. Circulo"
@@ -42,6 +52,7 @@ system("cls");
                //areas_(opc);
                 break;
                 case 2:
+                system("cls");
                 cout<<"Sub-menu volumenes"
             "\n1. Esfera"
             "\n2. Paralelepípedo"
@@ -49,7 +60,7 @@ system("cls");
             "\n4. Regresar al menú principal"
             "\nIngrese una opcion: ";
             cin>>opc;
-            //volumenes_(opc);
+            volumenes_(opc);
                 break;
                 case 3:
     cout<<"Sub-menu temperaturas"
@@ -75,50 +86,48 @@ system("cls");
                  //monedas_(opc);
                 break;
                 case 5:
-   cout<<"Ingrese la opcion a calcular"
-         "\n1. Convertir de decimal a binario"
-         "\n2. Convertir de decimal a octal"
-         "\n3. Convertir de decimal a hexadecimal"
-         "\n4. Convertir de binario a decimal"
+cout<<"Ingrese la opcion a calcular"
+             "\n1. Convertir de decimal a binario"
+             "\n2. Convertir de decimal a octal"
+             "\n3. Convertir de decimal a hexadecimal"
+             "\n4. Convertir de binario a decimal"
              "\n5. Convertir de octal a decimal"
              "\nIngrese una opcion: ";
         cin>>opc;
              //numericos_(opc);
                 break;
                 case 6:
-      cout<< "Ingrese la opcion a calcular"
-             "\n1. Ordenamiento de un arreglo"
-             "\n2. Buscar un valor en un arreglo"
-             "\n3. Suma de matrices"
-             "\n4. Multiplicación de matrices"
-             "\n5. Buscar un valor en una matriz"
-             "\n6. Regresar al menú principal"
-             "\nIngrese una opcion: ";
-              cin>>opc;
-              //arreglos_matrices(opc);
-             break;
+  cout<< "Ingrese la opcion a calcular"
+                 "\n1. Ordenamiento de un arreglo"
+                 "\n2. Buscar un valor en un arreglo"
+                 "\n3. Suma de matrices"
+                 "\n4. Multiplicación de matrices"
+                 "\n5. Buscar un valor en una matriz"
+                 "\n6. Regresar al menú principal"
+                 "\nIngrese una opcion: ";
+                 cin>>opc;
+                 //arreglos_matrices(opc);
+                break;
                 case 7:
-          cout<<"Sub-menu Varios"
-     "\n1. Distancia entre dos puntos"
-     "\n2. Raíces de una función cuadrática"
-     "\n3. Factorial"
-     "\n4. Fibonacci"
-     "\n5. Primo"
-     "\n6. Números amigos"
-     "\n7. Regresar al menú principal"
-     "\nIngrese una opcion: ";
-            cin>>opc;
-            //varios(opc);
+  cout<<"Sub-menu Varios"
+                 "\n1. Distancia entre dos puntos"
+                 "\n2. Raíces de una función cuadrática"
+                 "\n3. Factorial"
+                 "\n4. Fibonacci"
+                 "\n5. Primo"
+                 "\n6. Números amigos"
+                 "\n7. Regresar al menú principal"
+                 "\n Ingrese una opcion: ";
+                 cin>>opc;
+                 //varios(opc);
                 break;
                 case 8:
                system("cls");
                cout<<"Programa finalizado";
                 break;
                 default:
-                system("cls");
-                cout<<"Opcion invalida"<<endl;
-                system("pause");
-                main();
+                opc = 0;
+                opcion_invalida();
                 
                 
             }
@@ -126,4 +135,109 @@ system("cls");
             
                     
      return 0;    
+}
+
+
+
+void volumenes_(int &opc){
+    system("cls");
+if(opc >= 1 && opc <= 4){
+        if(opc==1){
+            float radio = 0;
+            cout<<"Ingrese el valor del radio de la esfera en cm:  ";
+            cin>>radio;
+            //volumen esfera
+            if(radio<=0){
+                cout<<"Valor invalido"<<endl;
+                
+                system("pause");
+                
+                volumenes_(opc);
+            }
+                   else{
+            
+                    radio = (4/3) * M_PI * (pow(radio, 3));
+                   cout<<"El volumen de la esfera es: "<<radio<<"cm³"<<endl;
+        
+                    system("pause");
+        
+                    opc=2;
+                    main();
+                    }
+        }
+        else if(opc ==2){
+            //volumen paralelepipedo
+            //ancho * longitud * altura
+         float ancho = 0, longitud = 0, altura = 0;
+            cout<<"Ingrese las medidas del paralelepipedo (cm) "
+            "\nAncho: ";
+            cin>>ancho;
+            cout<<"Altura: ";
+            cin>>altura;
+            cout<<"Longitud: ";
+            cin>>longitud;
+            if(ancho<=0 || altura <= 0 || longitud <=0){
+                cout<<"No puede ingresar valores negativos"<<endl;
+                system("pause");
+                opc = 2;
+                volumenes_(opc);
+            }
+            
+            cout<<"El volumen del paralelepipedo es: "<<ancho * longitud * altura<<"cm³"<<endl;
+        system("pause");
+        opc=2;
+        main();
+        }
+           else if(opc == 3){
+        //cubo
+        // lado³
+        float lado = 0;
+        cout<<"Ingrese la medida del cubo especificada (cm): "
+        "\nLado: ";
+        
+        cin>>lado;
+        if(lado<=0){
+                opc = 3;
+                cout<<"Valor invalido"<<endl;
+                system("pause");
+                volumenes_(opc);
+        }
+            else{
+                   cout<<"El volumen del cubo con lado "<<lado<<" es: "<<pow(lado, 3)<<"cm³"<<endl;
+                system("pause");
+                opc = 2;
+                main();
+                
+            }
+            }
+          else if(opc == 4){
+        opc=0;
+        main();
+    }
+    else {
+      opc = 2;
+      opcion_invalida();
+        
+    }
+ 
+
+    
+}
+else{
+    opc = 2;
+    opcion_invalida();
+   
+    
+}
+
+}
+
+
+
+
+void opcion_invalida(){
+    system("cls");
+    cout<<"Opcion invalida"<<endl;
+    system("pause");
+    main();
 }
